9878cb8c73bba54f0be1cc56e4d5ea17d3e7b1f9
Ian Denhardt <ian@zenhack.net>
2015-08-29 23:19:15 -0400

    Fix various pep8 violations

--- pgpmime/envelope.py
+++ pgpmime/envelope.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2011-2012  Patrick Totzke <patricktotzke@gmail.com>
+# Copyright (C) 2015       Ian Denhardt <ian@zenhack.net>
 # This file is released under the GNU GPL, version 3 or a later revision.
 # For further details see the COPYING file
 import os
@@ -25,8 +26,8 @@ def sign_payload(payload, key=None):
             plaintext, key)
         if len(signatures) != 1:
             raise GPGProblem("Could not sign message (GPGME "
-                                "did not return a signature)",
-                                code=GPGCode.KEY_CANNOT_SIGN)
+                             "did not return a signature)",
+                             code=GPGCode.KEY_CANNOT_SIGN)
     except gpgme.GpgmeError as e:
         if e.code == gpgme.ERR_BAD_PASSPHRASE:
             # If GPG_AGENT_INFO is unset or empty, the user just does
@@ -37,20 +38,22 @@ def sign_payload(payload, key=None):
                 raise GPGProblem(msg, code=GPGCode.BAD_PASSPHRASE)
             else:
                 raise GPGProblem("Bad passphrase. Is gpg-agent "
-                                    "running?",
-                                    code=GPGCode.BAD_PASSPHRASE)
+                                 "running?",
+                                 code=GPGCode.BAD_PASSPHRASE)
         raise GPGProblem(str(e), code=GPGCode.KEY_CANNOT_SIGN)
 
     micalg = crypto.RFC3156_micalg_from_algo(signatures[0].hash_algo)
-    unencrypted_msg = MIMEMultipart('signed', micalg=micalg,
-                                    protocol=
-                                    'application/pgp-signature')
+    unencrypted_msg = MIMEMultipart(
+        'signed',
+        micalg=micalg,
+        protocol='application/pgp-signature'
+    )
 
     # wrap signature in MIMEcontainter
     stype = 'pgp-signature; name="signature.asc"'
     signature_mime = MIMEApplication(_data=signature_str,
-                                        _subtype=stype,
-                                        _encoder=encode_7or8bit)
+                                     _subtype=stype,
+                                     _encoder=encode_7or8bit)
     signature_mime['Content-Description'] = 'signature'
     signature_mime.set_charset('us-ascii')
 
@@ -74,17 +77,17 @@ def encrypt_payload(unencrypted_msg, keys):
         raise GPGProblem(str(e), code=GPGCode.KEY_CANNOT_ENCRYPT)
 
     outer_msg = MIMEMultipart('encrypted',
-                                protocol='application/pgp-encrypted')
+                              protocol='application/pgp-encrypted')
 
     version_str = 'Version: 1'
     encryption_mime = MIMEApplication(_data=version_str,
-                                        _subtype='pgp-encrypted',
-                                        _encoder=encode_7or8bit)
+                                      _subtype='pgp-encrypted',
+                                      _encoder=encode_7or8bit)
     encryption_mime.set_charset('us-ascii')
 
     encrypted_mime = MIMEApplication(_data=encrypted_str,
-                                        _subtype='octet-stream',
-                                        _encoder=encode_7or8bit)
+                                     _subtype='octet-stream',
+                                     _encoder=encode_7or8bit)
     encrypted_mime.set_charset('us-ascii')
     outer_msg.attach(encryption_mime)
     outer_msg.attach(encrypted_mime)
