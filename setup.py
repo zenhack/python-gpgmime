c7f24ae231f03d4936bf35224a568844b605998b
Ian Denhardt <ian@zenhack.net>
2015-09-05 15:18:21 -0400

    Finish up setup.py/MANIFEST.in
    These are now complete enough to regenerate an sdist/upload to pypi.

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup
+from os.path import join, dirname
+
+
+def _get_readme():
+    with open(join(dirname(__file__), 'README.md')) as f:
+        return f.read()
+
 
 setup(name='gpgmime',
       version='0.1',
@@ -7,6 +14,13 @@ setup(name='gpgmime',
       author='Ian Denhardt',
       author_email='ian@zenhack.net',
       description='Library for manipulating PGP-Mime email.',
-      license='GPL-3.0',
+      long_description=_get_readme(),
+      license='GPLv3+',
       url='https://github.com/zenhack/python-pgpmime',
-      )
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+      ])
