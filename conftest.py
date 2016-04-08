3bfc7bf0c85dd25e462289753aed40be02927ba9
Ian Denhardt <ian@zenhack.net>
2016-04-07 21:30:52 -0400

    Integrate tox

---
+++ conftest.py
@@ -0,0 +1,3 @@
+# The presence of this file (even empty) causes python to include the
+# source directory in sys.path; this is important for being able to
+# import modules under testing/ from the tests.
